1cee2e56927963c6525e69e4e606eb1b649afe78
Crowdbotics <engineering@crowdbotics.com>
2023-08-08 07:45:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-fire-dev-108121",
+  url: "https://solitary-fire-dev-108121.botics.co",
+  api: "https://solitary-fire-dev-108121.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
